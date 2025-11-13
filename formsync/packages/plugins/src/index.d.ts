4ff38bbbee4a1f72ee318117e4d7de35eb915f5c
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-11-13 14:30:00 +0530

    Setting up the dependencies and packages

--- formsync/packages/plugins/src/index.d.ts
+++ formsync/packages/plugins/src/index.d.ts
@@ -1,5 +1,12 @@
+/**
+ * @formsync/plugins
+ *
+ * Shared plugin interfaces and utilities for FormSync Component 1
+ */
 export * from './interfaces/FormatParserPlugin';
 export * from './interfaces/SchemaValidatorPlugin';
 export * from './interfaces/LLMProviderPlugin';
+export * from './interfaces/BackendGeneratorPlugin';
 export * from './base/BasePlugin';
 export * from './PluginRegistry';
+//# sourceMappingURL=index.d.ts.map
