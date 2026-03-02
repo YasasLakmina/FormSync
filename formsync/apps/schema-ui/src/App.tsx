aab97a776b1810179dd19f34fb3398de59c1734e
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2026-03-02 10:20:00 +0530

    Created main layout and routing setup

--- formsync/apps/schema-ui/src/App.tsx
+++ formsync/apps/schema-ui/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
+import { AuthProvider } from './context/AuthContext';
 import { LandingPage } from './pages/LandingPage';
 import { EditorPage } from './pages/EditorPage';
 import { GeneratedCodePage } from './pages/GeneratedCodePage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { ProfilePage } from './pages/ProfilePage';
 import { Documentation } from './components/Documentation';
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
@@ -11,35 +14,38 @@ import { PageTransition } from './components/layout/PageTransition';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Router>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <Routes>
-            <Route 
-              path="/" 
+            <Route
+              path="/"
               element={
                 <PageTransition>
                   <LandingPage />
                 </PageTransition>
-              } 
+              }
             />
             <Route path="/editor" element={<><EditorPage /><Footer /></>} />
             <Route path="/documentation" element={<Documentation />} />
             <Route path="/generated" element={<><GeneratedCodePage /><Footer /></>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
+
+        {/* Toast Notifications */}
+        <Toaster
+          position="top-right"
+          expand={true}
+          richColors
+          closeButton
+        />
       </Router>
-      
-      {/* Toast Notifications */}
-      <Toaster 
-        position="top-right"
-        expand={true}
-        richColors
-        closeButton
-      />
-    </>
+    </AuthProvider>
   );
 }
 
